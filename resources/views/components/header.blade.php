8fb076932e1f289a84fe1e5e60e182b2f57e0674
zyxwars <zyxwars221@gmail.com>
2025-04-13 21:05:20 +0200

    Add simplified laravel breeze

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,9 +3,7 @@
     <div class="flex items-center justify-between">
         <div class="flex items-center">
             <!-- Logo -->
-            <a
-                href="/"
-                class="flex-none text-lg font-bold hover:line-through">
+            <a href="/" class="flex-none text-lg font-bold hover:line-through">
                 Record Store
             </a>
 
@@ -13,48 +11,47 @@
             <form action="{{ route('search') }}" method="GET" class="hidden md:block w-[32rem]">
                 <!-- https://daisyui.com/components/input/ -->
                 <label class="input ml-10 w-full">
-                        <svg
-                            class="h-[1em] opacity-50"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24">
-                            <g
-                                stroke-linejoin="round"
-                                stroke-linecap="round"
-                                stroke-width="2.5"
-                                fill="none"
-                                stroke="currentColor">
-                                <circle cx="11" cy="11" r="8"></circle>
-                                <path d="m21 21-4.3-4.3"></path>
-                            </g>
-                        </svg>
+                    <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
+                            stroke="currentColor">
+                            <circle cx="11" cy="11" r="8"></circle>
+                            <path d="m21 21-4.3-4.3"></path>
+                        </g>
+                    </svg>
 
-                        <input
-                            name='search'
-                            type="search"
-                            class="grow"
-                            placeholder="The Blues Ain't Never Gonna Die" />
+                    <input name='search' type="search" class="grow" placeholder="The Blues Ain't Never Gonna Die" />
                 </label>
             </form>
         </div>
 
         <!-- Action buttons -->
-        <div class="text-base-content flex-none">
-            <!-- https://daisyui.com/components/dropdown/ -->
-            <button
-                class="btn btn-ghost btn-circle"
-                popovertarget="popover-user"
-                style="anchor-name: --anchor-user">
-                <span class="material-symbols-outlined">person</span>
-            </button>
-            <ul
-                class="dropdown menu rounded-box bg-base-100 dropdown-center w-52 shadow-sm"
-                popover
-                id="popover-user"
-                style="position-anchor: --anchor-user">
-                <li>
-                    <a href="TODO">Logout</a>
-                </li>
-            </ul>
+        <div class="text-base-content flex-none flex items-center gap-2">
+            @if ($user != null)
+                <!-- https://daisyui.com/components/dropdown/ -->
+                <button class="btn btn-ghost btn-circle" popovertarget="popover-user"
+                    style="anchor-name: --anchor-user">
+                    <span class="material-symbols-outlined">person</span>
+                </button>
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <ul class="dropdown menu rounded-box bg-base-100 dropdown-center w-52 shadow-sm" popover
+                        id="popover-user" style="position-anchor: --anchor-user">
+                        <li>
+                            <a :href="route('logout')"
+                                onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                                Sign out
+                            </a>
+                        </li>
+                    </ul>
+                </form>
+            @else
+                <a href={{ route('login') }}>
+                    Sign In
+                </a>
+            @endif
 
             <a href="./cart.html" class="btn btn-ghost btn-circle">
                 <span class="material-symbols-outlined">shopping_cart</span>
@@ -66,25 +63,14 @@
     <form action="{{ route('search') }}" method="GET" class="md:hidden w-full">
         <!-- https://daisyui.com/components/input/ -->
         <label class="input w-full">
-            <svg
-                class="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24">
-                <g
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                    stroke-width="2.5"
-                    fill="none"
+            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none"
                     stroke="currentColor">
                     <circle cx="11" cy="11" r="8"></circle>
                     <path d="m21 21-4.3-4.3"></path>
                 </g>
             </svg>
-            <input
-                name='search'
-                type="search"
-                class="grow"
-                placeholder="The Blues Ain't Never Gonna Die" />
+            <input name='search' type="search" class="grow" placeholder="The Blues Ain't Never Gonna Die" />
         </label>
     </form>
 </nav>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<!-- https://github.com/laravel/breeze/blob/2.x/stubs/default/resources/views/auth/login.blade.php -->
+
+<x-layout>
+    <x-header></x-header>
+
+    <main class="mt-8 flex items-start justify-center p-4">
+        <form class="grid gap-2 max-w-64" method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <h1 class="mb-2 text-2xl font-bold">Sign in</h1>
+
+            <input placeholder="Email" class="input" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+            <input class='input' type="password" name='password' placeholder="Password" required
+                autocomplete="current-password" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
+            <!-- TODO: change styling -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox"
+                        class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                        name="remember">
+                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                </label>
+            </div>
+
+            <button class="btn btn-primary mt-2" type="submit">
+                Sign in
+            </button>
+
+            <div class="mt-2 justify-self-center">
+                Don't have an account?
+                <a class="font-bold underline" href="{{ route('register') }}">Sign up</a>
+            </div>
+
+        </form>
+    </main>
+</x-layout>
