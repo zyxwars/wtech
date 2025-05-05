5c22db12285ba7f5db65a0cfa2902f4dcb57e239
MichalWeis <misko@weis.sk>
2025-05-05 12:06:33 +0200

    Added new migration that adds is_admin column to users table, create IsAdmin middleware to check if user is admin and registered it globaly in app.php

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Support\Facades\Auth)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (! Auth::check() || ! Auth::user()->is_admin) {
+            abort(403, 'Unauthorized action.');
+        }
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\IsAdmin;
+use GuzzleHttp\Promise\Is;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(IsAdmin::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
